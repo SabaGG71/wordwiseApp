258c5b16c8162bda10daf4ccfe890b0de8c9ebad
Saba <sgachechiladze881@gmail.com>
2024-11-04 01:38:18 +0400

    uiii

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
   resolve: {
     alias: {
